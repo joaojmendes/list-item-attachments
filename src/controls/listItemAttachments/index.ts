97e09ce6fb00da9a46d3580ed1a173688f436be6
João <joao.j.mendes@outlook.com>
2018-11-02 15:16:46 +0000

    Initial Commit

---
+++ src/controls/listItemAttachments/index.ts
@@ -0,0 +1,4 @@
+// A file is required to be in the root of the /src directory by the TypeScript compiler
+export * from './IListItemAttachmentsProps';
+export * from './IListItemAttachmentsState';
+export * from './ListItemAttachments';
--- src/controls/listItemPicker/index.ts
+++
@@ -1,4 +0,0 @@
-// A file is required to be in the root of the /src directory by the TypeScript compiler
-export * from './IListItemPickerProps';
-export * from './IListItemPickerState';
-export * from './ListItemPicker';
